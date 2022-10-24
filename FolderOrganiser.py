265a13b410e74599eca53d8b6fd46a4e429c36ef
chri5toph3r <kris_kulak@wp.pl>
2022-10-24 18:43:22 +0200

    import cleanup and cosmetic changes

--- FolderOrganiser.py
+++ FolderOrganiser.py
@@ -1,11 +1,11 @@
-import os
-import shutil
+from os import path, mkdir, listdir
+from shutil import move
 
 
 # get path and list of files
 def get_f_d_list(dir_name=None, get_dirs=False, tabs=0):
-    if not os.path.isdir(dir_name):
-        dir_name = os.path.dirname(__file__).replace("\\", "/")
+    if not path.isdir(dir_name):
+        dir_name = path.dirname(__file__).replace("\\", "/")
         print(f"Directory not given/found")
 
     for t in range(tabs):
@@ -13,11 +13,11 @@ def get_f_d_list(dir_name=None, get_dirs=False, tabs=0):
     print(f"Current directory: {dir_name}/\n")
 
     try:
-        elements_list = os.listdir(dir_name)
-        files_list = [f for f in elements_list if os.path.isfile(f"{dir_name}/{f}") and "." in f]
+        elements_list = listdir(dir_name)
+        files_list = [f for f in elements_list if path.isfile(f"{dir_name}/{f}") and "." in f]
 
         if get_dirs:
-            dirs_list = [d for d in elements_list if os.path.isdir(f"{dir_name}/{d}")]
+            dirs_list = [d for d in elements_list if path.isdir(f"{dir_name}/{d}")]
             return dir_name, files_list, dirs_list
         return dir_name, files_list
     except IOError:
@@ -50,13 +50,13 @@ def check_asked_extensions(ext_list, extensions):
 # get destination directory and transfer files
 def check_dir_transfer_f(dir_path, dest_dir, files_list, ext_to_move_list):
     dest_path = f"{dir_path}/{dest_dir}"
-    if not os.path.exists(dest_path):
+    if not path.exists(dest_path):
         print(f"Creating directory: {dest_path}")
-        os.mkdir(dest_path)
+        mkdir(dest_path)
         print("Directory created")
-    elif not os.path.isdir(dest_path):
+    elif not path.isdir(dest_path):
         print(f"Creating directory: {dest_path}")
-        os.mkdir(dest_path)
+        mkdir(dest_path)
         print("Directory created")
     else:
         print("Directory found")
@@ -64,20 +64,20 @@ def check_dir_transfer_f(dir_path, dest_dir, files_list, ext_to_move_list):
     moved_files = 0
     for file in files_list:
         if file.split(".")[-1] in ext_to_move_list:
-            shutil.move(f"{dir_path}/{file}", f"{dest_path}/{file}")
+            move(f"{dir_path}/{file}", f"{dest_path}/{file}")
             moved_files += 1
 
     return moved_files
 
 
 if __name__ == '__main__':
-    files_path = os.path.dirname(__file__).replace('\\', '/')
+    files_path = path.dirname(__file__).replace('\\', '/')
     print(f"File's path: {files_path}")
-    path = input("path: ")
-    path, f_list = get_f_d_list(path)
+    chosen_path = input("path: ")
+    chosen_path, f_list = get_f_d_list(chosen_path)
     all_e_list = get_all_file_extensions(f_list)
     e_list = check_asked_extensions(all_e_list, input("extensions: "))
 
-    d_dir = input(f"Destination: {path}/")
-    print(f"{check_dir_transfer_f(path, d_dir, f_list, e_list)} files moved successfully")
+    d_dir = input(f"Destination: {chosen_path}/")
+    print(f"{check_dir_transfer_f(chosen_path, d_dir, f_list, e_list)} files moved successfully")
     input("Press Enter to exit...")
--- LargestFiles.py
+++ LargestFiles.py
@@ -1,4 +1,4 @@
-import os
+from os import stat, path
 import FolderOrganiser
 
 
@@ -19,14 +19,14 @@ def print_out(input_list, cur_path, info, blank_line=False, tabs=0):
     return False
 
 
-def set_path(cur_path, files_sizes, min_mb, tabs=0):
+def set_path(cur_path, files_sizes, min_mb=0.0, tabs=0):
     cur_path, f_list, d_list = FolderOrganiser.get_f_d_list(cur_path, True, tabs=tabs)
     are_files = print_out(f_list, cur_path, "files", tabs=tabs)
     are_dirs = print_out(d_list, cur_path, "folders", are_files, tabs)
 
     if are_files:
         for f in f_list:
-            file_stats = os.stat(f"{cur_path}/{f}")
+            file_stats = stat(f"{cur_path}/{f}")
             file_size_mb = round((file_stats.st_size / (1024 * 1024)), 2)
             if file_size_mb > min_mb:
                 files_sizes.append((file_size_mb, f"{cur_path}/{f}"))
@@ -39,13 +39,17 @@ def set_path(cur_path, files_sizes, min_mb, tabs=0):
 
 
 if __name__ == '__main__':
-    files_path = os.path.dirname(__file__).replace('\\', '/')
+    files_path = path.dirname(__file__).replace('\\', '/')
     print(f"File's path: {files_path}")
-    path = input("path: ")
+    chosen_path = input("path: ")
+
     min_f_size = input("smallest file size to intake (in MB): ")
+    if not min_f_size:
+        min_f_size = 0.0
+        print(f"smallest file size to intake set to default ({min_f_size})")
 
     f_sizes = []
-    f_sizes = set_path(path, f_sizes, float(min_f_size))
+    f_sizes = set_path(chosen_path, f_sizes, float(min_f_size))
     f_sizes.sort(reverse=True)
 
     sizes_sum = 0
